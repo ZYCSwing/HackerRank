c60a4e614e92a9b35459f70a814b87541d0ad9d0
Swing <coco06267@163.com>
2018-05-07 14:47:07 +0800

    https://www.hackerrank.com/challenges/benders-play/problem

---
+++ Game Theory/Play on benders.cpp
@@ -0,0 +1,161 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <stack>
+#include <algorithm>
+#include <cmath>
+#include <string>
+#include <cstring>
+#include <map>
+#include <set>
+#include <bitset>
+#include <sstream>
+#include <cstdlib>
+#include <unordered_map>
+#include <unordered_set>
+#include <list>
+#include <climits>
+#include <cstdio>
+#include <ctype.h>
+#include <time.h>
+using namespace std;
+
+#define PI acos(-1.0)
+#define EPS 1e-8
+#define MOD 1000000007
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define pb push_back
+#define fi first
+#define se second
+#define go(i, a, b) for(int i = a; i <= b; ++i)
+#define gorev(i, a, b) for(int i = a; i >= b; --i)
+
+typedef long double ld;
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef vector<int> vi;
+typedef vector<vector<int>> vvi;
+
+template <class L, class R> ostream &operator <<(ostream &os, pair<L, R> P){
+    return os << "(" << P.fi << "," << P.se << ")";
+}
+template <class T> ostream &operator <<(ostream &os, vector<T> V){
+    os << "["; for (auto vv: V) os << vv << ","; return os << "]";
+}
+template <class L, class R> ostream &operator <<(ostream &os, map<L, R> M){
+    os << "["; for (auto pr: M) os << pr.fi << "->" << pr.se << ","; return os << "]";
+}
+template <class L, class R> ostream &operator <<(ostream &os, unordered_map<L, R> M){
+    os << "["; for (auto pr: M) os << pr.fi << "->" << pr.se << ","; return os << "]";
+}
+
+struct Matrix {
+    vvi data;
+    int r, c;
+    Matrix(int row, int col, bool identity = false) : r(row), c(col) {
+        data.assign(row, vector<int>(col, 0));
+        if (identity) {
+            for (int i = 0; i < r; ++i) {
+                data[i][i] = 1;
+            }
+        }
+    }
+    Matrix operator * (Matrix& other) {
+        int m = r, n = c, p = other.c;
+        Matrix res(m, p);
+        for (int i = 0; i < m; ++i) {
+            for (int j = 0; j < p; ++j) {
+                for (int k = 0; k < n; ++k) {
+                    res.data[i][j] += data[i][k] * other.data[k][j];
+                }
+            }
+        }
+        return res;
+    }
+};
+
+vector<int> extendGcd(int a, int b) {
+    if (b == 0) {
+        return {1, 0, a};
+    } else {
+        vector<int> tmp = extendGcd(b, a % b);
+        return {tmp[1], tmp[0] - (a / b) * tmp[1], tmp[2]};
+    }
+}
+
+Matrix matrix_power(Matrix base, ll exp) {
+    int n = base.r;
+    Matrix res(n, n, true);
+    while (exp) {
+        if (exp & 1) {
+            res = res * base;
+        }
+        base = base * base;
+        exp >>= 1;
+    }
+    return res;
+}
+
+/******************************** template ********************************/
+#define N 100010
+
+int n, m;
+vi edges[N];
+int sg[N];
+
+int solve(int cur) {
+    if (sg[cur] != -1) return sg[cur];
+    if (edges[cur].empty()) return sg[cur] = 0;
+    int tmp[N];
+    memset(tmp, 0, sizeof(tmp));
+
+    for (int next : edges[cur]) {
+        tmp[solve(next)] = 1;
+    }
+
+    for (int i = 0; i < N; ++i) {
+        if (!tmp[i]) return sg[cur] = i;
+    }
+
+    return -1;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+    cout.precision(10);
+    cout << fixed;
+#ifdef ZYC_LOCAL
+    freopen("input.txt", "rt", stdin);
+#endif
+    cin >> n >> m;
+    int u, v;
+    go(i, 1, m) {
+        cin >> u >> v;
+        edges[u].push_back(v);
+    }
+
+    memset(sg, -1, sizeof(sg));
+    go(i, 1, n) {
+        solve(i);
+    }
+
+    int q, k;
+    cin >> q;
+    while (q--) {
+        cin >> k;
+        int res = 0;
+        go(i, 1, k) {
+            cin >> u;
+            res ^= sg[u];
+        }
+        cout << (res ? "Bumi" : "Iroh") << endl;
+    }
+
+#ifdef ZYC_LOCAL
+    cerr << "Time elapsed: " << 1.0 * clock() / CLOCKS_PER_SEC << " s.\n";
+#endif
+    return 0;
+}
